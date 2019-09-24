92d3dd02fe9889db3aad9caef1954f1f34d5acc1
Tyler Torres <torrest123@gmail.com>
2019-09-24 19:06:41 -0400

    fixed keep username when error and fixed welcome

--- main.py
+++ main.py
@@ -5,43 +5,47 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-name_error = ''
+name_error = 'Please enter a valid username'
+pass1_error = 'Please enter a valid password'
+pass2_error = 'Password does not match'
 
 @app.route('/')
 def index():
     return render_template('sign-up.html')
 
-@app.route('/welcome', methods=['POST'])
-def signed_in():
-    username = request.form['username']
-    username= str(username)
-    pass1 = request.form['pass1']
-    pass1 = str(pass1)
-    pass2 = request.form['pass2']
-    pass2 = str(pass2)
-    email = request.form['email']
+@app.route('/', methods=['POST', 'GET'])
+def sign_up():
+    username = str(request.form['username'])
+    pass1 = str(request.form['pass1'])
+    pass2 = str(request.form['pass2'])
+    email = str(request.form['email'])
 
     if (not username) or (username.strip() == ""):
-        return redirect('/')
+        return render_template('sign-up.html', name_error=name_error)
     
     if (not pass1) or (pass1.strip() == ''):
-        return redirect('/')
+        return render_template('sign-up.html', pass1_error=pass1_error, username=username)
     
     if (not pass2) or (pass2.strip() == ''):
-        return redirect('/')
+        return render_template('sign-up.html', pass2_error=pass2_error, username=username)
 
     if len(username) < 3 or len(username) > 20:
-        return redirect('/')
+        return render_template('sign-up.html', name_error=name_error, username=username)
 
     if len(pass1) < 3 or len(pass1) > 20:
-        return redirect('/')
+        return render_template('sign-up.html', pass1_error=pass1_error, username=username)
 
     if len(pass2) < 3 or len(pass2) > 20:
-        return redirect('/')
+        return render_template('sign-up.html', pass2_error=pass2_error, username=username)
 
     if pass1 != pass2:
-        return redirect('/')
-    
+        return render_template('sign-up.html', pass2_error=pass2_error, username=username)
+
+    return redirect('/welcome?username=' + username)
+
+@app.route('/welcome')
+def signed_in():
+    username = request.args.get('username')
     return render_template('welcome.html', username=username)
 
 app.run()
