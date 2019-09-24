44f0b7d10dac91181aa5e9263f72c7429c0e4006
Tyler <torrest123@gmail.com>
2019-09-24 16:25:37 -0400

    Handle errors (no error messages yet)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+name_error = ''
+
 @app.route('/')
 def index():
     return render_template('sign-up.html')
@@ -12,9 +14,33 @@ def index():
 @app.route('/welcome', methods=['POST'])
 def signed_in():
     username = request.form['username']
+    username= str(username)
     pass1 = request.form['pass1']
+    pass1 = str(pass1)
     pass2 = request.form['pass2']
+    pass2 = str(pass2)
     email = request.form['email']
+
+    if (not username) or (username.strip() == ""):
+        return redirect('/')
+    
+    if (not pass1) or (pass1.strip() == ''):
+        return redirect('/')
+    
+    if (not pass2) or (pass2.strip() == ''):
+        return redirect('/')
+
+    if len(username) < 3 or len(username) > 20:
+        return redirect('/')
+
+    if len(pass1) < 3 or len(pass1) > 20:
+        return redirect('/')
+
+    if len(pass2) < 3 or len(pass2) > 20:
+        return redirect('/')
+
+    if pass1 != pass2:
+        return redirect('/')
     
     return render_template('welcome.html', username=username)
 
